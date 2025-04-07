08da4e483db6f4746569d19d426ee27e3f98f3d3
sanjidaRimi023 <sanjidarimi023@gmail.com>
2025-04-07 23:24:55 +0600

    add btn

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -1,18 +1,21 @@
-import React, { use } from 'react';
-import Country from '../Country/Country';
+import React, { use } from "react";
+import Country from "../Country/Country";
+import "./Countries.css";
 
-const Countries = ({countriesPromise}) => {
-    const countries = use(countriesPromise);
-    console.log(countries);
+const Countries = ({ countriesPromise }) => {
+  const countries = use(countriesPromise);
+//   console.log(countries);
 
-    return (
-        <div>
-            <h1>Travel on world : {countries.length}</h1>
-            {
-                countries.map(country => <Country key={country.cca3} country = {country}></Country>)
-            }
-        </div>
-    );
+  return (
+    <div>
+      <h1>Travel on world : {countries.length}</h1>
+      <div className="countries">
+        {countries.map((country) => (
+          <Country key={country.cca3} country={country}></Country>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Countries;
--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,14 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './Country.css'
 
 const Country = ({country}) => {
-    console.log(country);
+
+    const [visited , setVisited] = useState(false);
+
+    
+
+    const handleVisit = () =>{
+        // if(visited === true){
+        //     setVisited(false);
+        // }else{
+        //     setVisited(true);
+        // }
+        setVisited(!visited)
+    }
+    
     return (
-        <div>
+        <div className={`country ${visited && 'country-visited'}`}>
             <h2>Name : {country.name.common}</h2>
             <h4>official Name : {country.name.official}</h4>
             <p>Independent : {country.independent ? 'independent country' : 'Not independent country'}</p>
             <p>Population : {country.population}</p>
-            <img src={country.flags.png} alt="" />
+            <img src={country.flags?.png} alt="" />
+            <button className={visited ? 'btn-visited' : 'btn-not-visited'} onClick={handleVisit}>{visited ? 'Visited' : 'Not Visited'}</button>
+           
         </div>
     );
 };
